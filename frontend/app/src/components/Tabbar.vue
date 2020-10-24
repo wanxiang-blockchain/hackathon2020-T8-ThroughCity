7fc62ef64fb9be9cb1d5fae671965182b837b219
dengli <dengli@wxblockchain.com>
2020-10-24 13:54:47 +0800

    h5项目初始化

---
+++ frontend/app/src/components/Tabbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="tabbar">
+        <ul>
+            <li></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'tabbar',
+    data() {
+        return {
+            tabbar: [
+                {
+
+                },
+                {
+                    
+                }
+            ]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
