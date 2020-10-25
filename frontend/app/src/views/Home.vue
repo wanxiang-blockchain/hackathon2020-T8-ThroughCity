17be88659c5916a8c9f5404eb74b5b80f28e651b
dengzhiyuan <dengzhjiyuan@wxblockchian.com>
2020-10-25 09:51:43 +0800

    add 下拉刷新

--- frontend/app/src/views/Home.vue
+++ frontend/app/src/views/Home.vue
@@ -1,39 +1,28 @@
 <template>
   <div class="home">
-    <van-notice-bar left-icon="volume-o" text="In Blokchcain We Trust" />
-    <div class="card-wrapper">
-      <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-        <van-swipe-item class="banner">
-          <div class="left">
-            <van-image
-              width="3rem"
-              height="3rem"
-              fit="cover"
-              src="https://fang-oss.haozu.com/app/index/2020/09/17/ol61af2ihi.jpg?x-oss-process=image/resize,m_fill,w_492,h_356/quality,Q_90/crop,g_center,x_0,y_0,h_356,w_492"
-            />
-          </div>
-          <div class="right">
-            <ul>
-              <li class="space">空间： 华虹国际大厦 9层</li>
-              <li>日租金： 900token</li>
-              <li>押金： 27000token</li>
-              <li>到期日： 2022-10-24日</li>
-            </ul>
-          </div>
-        </van-swipe-item>
-      </van-swipe>
-    </div>
-    <h4>数字园区</h4>
-    <div class="card-wrapper">
-      <div class="icon-list-wrapper">
-        <ul class="icon-list">
-          <li class="item" v-for="item in park" :key="item.name">
-            <svg class="icon" aria-hidden="true">
-              <use :xlink:href="item.icon"></use>
-            </svg>
-            <span class="text">{{ item.name }}</span>
-          </li>
-        </ul>
+    <van-pull-refresh v-model="isLoading" success-text="刷新成功" @refresh="onRefresh">
+      <van-notice-bar left-icon="volume-o" text="In Blokchcain We Trust" />
+      <div class="card-wrapper">
+        <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+          <van-swipe-item class="banner">
+            <div class="left">
+              <van-image
+                width="3rem"
+                height="3rem"
+                fit="cover"
+                src="https://fang-oss.haozu.com/app/index/2020/09/17/ol61af2ihi.jpg?x-oss-process=image/resize,m_fill,w_492,h_356/quality,Q_90/crop,g_center,x_0,y_0,h_356,w_492"
+              />
+            </div>
+            <div class="right">
+              <ul>
+                <li class="space">空间： 华虹国际大厦 9层</li>
+                <li>日租金： 900token</li>
+                <li>押金： 27000token</li>
+                <li>到期日： 2022-10-24日</li>
+              </ul>
+            </div>
+          </van-swipe-item>
+        </van-swipe>
       </div>
       <h4>数字园区</h4>
       <div class="card-wrapper">
@@ -47,33 +36,46 @@
             </li>
           </ul>
         </div>
-      </div>
-      <h4>积分服务</h4>
-      <div class="card-wrapper">
-        <div class="icon-list-wrapper">
-          <ul class="icon-list">
-            <li class="item" v-for="item in token" :key="item.name">
-              <svg class="icon" aria-hidden="true">
-                <use :xlink:href="item.icon" />
-              </svg>
-              <span class="text">{{ item.name }}</span>
+        <h4>数字园区</h4>
+        <div class="card-wrapper">
+          <div class="icon-list-wrapper">
+            <ul class="icon-list">
+              <li class="item" v-for="item in park" :key="item.name">
+                <svg class="icon" aria-hidden="true">
+                  <use :xlink:href="item.icon" />
+                </svg>
+                <span class="text">{{ item.name }}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <h4>积分服务</h4>
+        <div class="card-wrapper">
+          <div class="icon-list-wrapper">
+            <ul class="icon-list">
+              <li class="item" v-for="item in token" :key="item.name">
+                <svg class="icon" aria-hidden="true">
+                  <use :xlink:href="item.icon" />
+                </svg>
+                <span class="text">{{ item.name }}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <h4>附近</h4>
+        <div class="card-wrapper">
+          <ul class>
+            <li v-for="item in activity" :key="item.name">
+              <van-card
+                :price="item.amount"
+                :desc="item.content"
+                :title="item.name"
+                thumb="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2307976301,4112990994&fm=26&gp=0.jpg"
+              />
+              <van-divider />
             </li>
           </ul>
         </div>
-      </div>
-      <h4>附近</h4>
-      <div class="card-wrapper">
-        <ul class>
-          <li v-for="item in activity" :key="item.name">
-            <van-card
-              :price="item.amount"
-              :desc="item.content"
-              :title="item.name"
-              thumb="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2307976301,4112990994&fm=26&gp=0.jpg"
-            />
-            <van-divider />
-          </li>
-        </ul>
       </div>
     </van-pull-refresh>
   </div>
